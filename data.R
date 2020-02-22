f991969de8212f1d2c06df6866f76f5459482365
ethanbartlow <59845251+ethanbartlow@users.noreply.github.com>
2020-02-21 20:50:56 -0800

    Update data.R

--- data.R
+++ data.R
@@ -2,4 +2,4 @@ library("dplyr")
 
 # Global variables
 df_inc_by_loc <- read.csv("data/datausa.io/Income by Location.csv")
-df_inc_by_loc <- read.csv("data/kaggle.com/US Adult Income")
+df_inc_by_loc <- read.csv("data/kaggle.com/adult-training.csv")
