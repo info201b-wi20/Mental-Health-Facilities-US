771de6df2b7c050eb572556f589d9112230a99fb
Ann Tseng <tsenga11@uw.edu>
2020-02-25 16:00:06 -0800

    added canvas instructions to each file

---
+++ Scripts/table_of_summary.R
@@ -0,0 +1,7 @@
+# Aggregate Table of Summary criteria: (Ann's part)
+# The second file you should save in your scripts/ directory should contain a function that takes in a dataset as a parameter, and returns a table of aggregate information about it. It must perform a groupby operation to show a dimension of the dataset as grouped by a particular feature (column). We expect the included table to:
+  
+# - Have well formatted column names
+# - Only contain relevant information
+# - Be intentionally sorted in a meaningful way
+# You must also describe why you included the table, and what information it reveals.
---
+++ Scripts/third_chart.R
@@ -0,0 +1,9 @@
+# Chart Script(s) (Austin's part)
+# You should write functions that take in your dataset as a parameter, and return a visualization of that data. 
+# This will allow you to call your charting function from your index.Rmd file, keeping your code much easier to read and update. Create a separate .R file for each chart.
+
+# Each function must return a different type of visualization (e.g., you can only create one Scatter Plot).  
+# Again, in your report you must describe why you included the chart (e.g., what it attempts to seeks to express), and what information it reveals. For each chart, we expect the following:
+  
+# Proper labels/titles/legends
+# Intentional chart type and encoding selection based on the question of interest and data type
