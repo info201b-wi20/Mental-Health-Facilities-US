96e6870220a31ee4d5d967597f649e600937dfbd
seasonphia <sxw2@uw.edu>
2020-02-29 16:59:35 -0800

    added summary table csv with new summary functions

--- Scripts/calculate_Summary_info.R
+++ Scripts/calculate_Summary_info.R
@@ -1,6 +1,20 @@
 # Summary Information Script (Sophia's part)
 # The first file you should save in your scripts/ directory should contain a function that takes in a dataset as a parameter, 
 # and returns a list of information about it. For example:
+
+library(styler)
+library(lintr)
+library(dplyr)
+library(tidyr)
+library(knitr)
+
+mental_health_resources <- read.csv("../Data/mental_health_summary_table.csv",
+  stringsAsFactors = FALSE)
+  
+state_highest_mental_health_resources <- mental_health_resources %>%
+  filter(Offer_diagnostic_evaluations ==
+  max(Offer_diagnostic_evaluations, na.rm = TRUE)) %>%
+  pull(State)
   
   # A function that takes in a dataset and returns a list of info about it:
   get_summary_info <- function(dataset) {
