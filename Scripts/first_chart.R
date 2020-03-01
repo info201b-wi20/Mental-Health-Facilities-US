d93a0f2e8599094ddcf5b3b0d02b6cd631d9c799
Ann Tseng <tsenga11@uw.edu>
2020-02-29 17:24:35 -0800

    testing to see if my commits would be added

--- Scripts/first_chart.R
+++ Scripts/first_chart.R
@@ -5,3 +5,95 @@
   
 # Proper labels/titles/legends
 # Intentional chart type and encoding selection based on the question of interest and data type
+
+summary_table <- 
+  read.csv("../Data/mental_health_data.csv", stringsAsFactors = FALSE) %>%
+  select(
+    LST, 
+    MHDIAGEVAL, 
+    FACILITYTYPE, 
+    FOCUS, 
+    OWNERSHP, 
+    NOTREAT, 
+    SIGNLANG, 
+    LANGPROV, 
+    QUALREV, 
+    OUTFUP) %>%
+  within(MHDIAGEVAL <- factor(MHDIAGEVAL, labels = c("No", "Yes"))) %>%
+  within(FACILITYTYPE <- factor(FACILITYTYPE, labels = c(
+    "Psychiatric hospital", 
+    "Separate inpatient psychiatric unit of a general hospital", 
+    "Residential treatment center for children",
+    "Residential treatment center for adults", 
+    "Other type of residential treatment facility",
+    "Veterans Administration medical center (VAMC)",
+    "Community mental health center (CMHC)",
+    "Partial hospitalization/day treatment facility",
+    "Outpatient mental health facility",
+    "Multi-setting mental health facility",
+    "Other"
+  ))) %>%
+  within(FOCUS <- factor(FOCUS, labels = c(
+    "Mental health treatment",
+    "Mix of mental health and substance abuse treatment",
+    "General health care",
+    "Other service focus"
+  ))) %>%
+  within(OWNERSHP <- factor(OWNERSHP, labels = c(
+    "Private for-profit organization",
+    "Private non-profit organization",
+    "Public agency or department"
+  ))) %>%
+  within(NOTREAT <- factor(NOTREAT, labels = c(
+    "Missing",
+    "No",
+    "Yes"
+  ))) %>%
+  within(SIGNLANG <- factor(SIGNLANG, labels = c(
+    "Missing",
+    "No",
+    "Yes"
+  ))) %>%
+  within(LANGPROV <- factor(LANGPROV, labels = c(
+    "NA",
+    "Missing",
+    "Staff who speak a language other than English",
+    "On-call interpreter brought in when needed",
+    "Both staff and on-call interpreter"
+  ))) %>%
+  within(QUALREV <- factor(QUALREV, labels = c(
+    "Missing",
+    "No",
+    "Yes"
+  ))) %>%
+  within(OUTFUP <- factor(OUTFUP, labels = c(
+    "Missing",
+    "No",
+    "Yes"
+  ))) %>%
+  rename(
+    "State" = LST,
+    "Offer_diagnostic_evaluations" = MHDIAGEVAL,
+    "Facility" = FACILITYTYPE,
+    "Primary_treatment_focus" = FOCUS,
+    "Organization_type" = OWNERSHP,
+    "NO_identified_mental_health_treatment_approaches" = NOTREAT,
+    "Provides_sign_language" = SIGNLANG,
+    "Provides_languages_other_than_English" = LANGPROV,
+    "Regularly_scheduled_quality_reviews" = QUALREV,
+    "Regularly_followup_after_discharge" = OUTFUP
+  ) %>%
+  group_by(State) %>%
+  summarize(
+    Offer_diagnostic_evaluations = 
+      sum(Offer_diagnostic_evaluations == "Yes", 
+          na.rm = TRUE),
+    NO_identified_mental_health_treatment_approaches =
+      sum(NO_identified_mental_health_treatment_approaches == "Yes",
+          na.rm = TRUE),
+    Provides_sign_language = 
+      sum(Provides_sign_language == "Yes", na.rm = TRUE),
+    Regularly_scheduled_quality_reviews =
+      sum(Regularly_scheduled_quality_reviews == "Yes", na.rm = TRUE),
+    Regularly_followup_after_discharge =
+      sum(Regularly_followup_after_discharge == "Yes", na.rm = TRUE))
