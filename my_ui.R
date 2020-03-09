0cdfd719f46fe8b2ef372384dbd82dc7a819741a
Austin Jenchi <ajenchi9@gmail.com>
2020-03-09 16:38:35 -0700

    add a chocolate animation

--- my_ui.R
+++ my_ui.R
@@ -43,5 +43,27 @@ my_ui <- navbarPage(
   "Mental Health Facilities Across the US",
   first_page,
   second_page,
-  third_page
+  third_page,
+  footer = HTML("
+    <img src='https://go.aws/38xnkMQ' alt='chocolate bar' id='chocolate'>
+    <style>
+      #chocolate {
+        animation: footer 10s infinite ease-out;
+        width: 100px;
+        position: fixed;
+        bottom: 16px;
+      }
+      
+      @keyframes footer {
+        0%, 10%, 90%, 100% {
+          margin-left: 8px;
+          transform: rotate(0deg) rotateY(0deg);
+        }
+        40%, 60% {
+          margin-left: calc(100% - 108px);
+          transform: rotate(720deg) rotateY(360deg);
+        }
+      }
+    </style>
+  ")
 )
