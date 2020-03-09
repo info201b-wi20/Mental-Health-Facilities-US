712c13ed07fd4da17a9f1c149657cbefa70ac73f
seasonphia <sxw2@uw.edu>
2020-03-09 15:56:13 -0700

    pushing formatted pages

--- my_ui.R
+++ my_ui.R
@@ -3,3 +3,20 @@ library(lintr)
 library(dplyr)
 library(ggplot2)
 library(shiny)
+
+# Pages for Shiny Application
+first_page <- tabPanel(
+  "",
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      h4(""),
+      plotOutput("")
+    )
+  )
+)
+
+ui <- navbarPage(
+  "Mental Health Facilities Across the US",
+  first_page
+)
