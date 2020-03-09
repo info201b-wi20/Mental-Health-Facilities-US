14fc270d900208979d37e2b6df99ad600e8fb175
Austin Jenchi <ajenchi9@gmail.com>
2020-03-09 16:08:37 -0700

    render plotly

--- my_ui.R
+++ my_ui.R
@@ -3,20 +3,45 @@ library(lintr)
 library(dplyr)
 library(ggplot2)
 library(shiny)
+library(plotly)
 
 # Pages for Shiny Application
 first_page <- tabPanel(
-  "",
+  "A",
   sidebarLayout(
     sidebarPanel(),
     mainPanel(
-      h4(""),
-      plotOutput("")
+      h4("A"),
+      plotlyOutput("firstChart")
     )
   )
 )
 
-ui <- navbarPage(
+second_page <- tabPanel(
+  "B",
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      h4("B"),
+      plotlyOutput("secondChart")
+    )
+  )
+)
+
+third_page <- tabPanel(
+  "C",
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      h4("C"),
+      plotlyOutput("thirdChart")
+    )
+  )
+)
+
+my_ui <- navbarPage(
   "Mental Health Facilities Across the US",
-  first_page
+  first_page,
+  second_page,
+  third_page
 )
